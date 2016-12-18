68dbe8876ebf37ad8678969e610e43a17b5030b1
jjr2930 <theoowwp@naver.com>
2016-12-18 21:10:19 +0900

    Create Interface  JIUIManager.cs all UIManager is Inherited JIUIManager.cs

---
+++ Assets/Scripts/UIScene/ExitUIManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using JLib;
+public class ExitUIManager : JMonoBehaviour, JLib.JIUIManager
+{
+    public void Awake()
+    {
+        Debug.Log( "ExitUIManager Awake is nothing to do" );
+    }
+
+    public void SetActive( bool value )
+    {
+        gameObject.SetActive( false );
+    }
+}
+
