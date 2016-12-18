0992be52800b41357d96224e0f294655096d7310
jjr2930 <theoowwp@naver.com>
2016-12-18 21:05:09 +0900

    DogEnum 통합

--- Assets/Scripts/DogEnums.cs
+++ Assets/Scripts/DogEnums.cs
@@ -18,15 +18,63 @@ public enum DoAction
     배변치우기
 }
 
+public enum DogDesire
+{
+    배고픔,
+    심심함,
+    더러움,
+    졸림,
+    똥마려움,
+}
 public enum UIID
 {
     None,
+    //Menu
+    시작,
+    개발자,
+    종료,
+
+    //Ingame
     밥먹이기버튼,
     재우기버튼,
     씻기기버튼,
     놀아주기버튼,
     배변치우기버튼,
     옷입히기버튼
-}
 
     
+}
+
+public enum DogUI
+{
+    None,
+    ShowCommonPopup,
+}
+
+
+
+
+public enum SmartObjectType
+{
+    None = 0,
+    Sleep,
+    Feed,
+    Pee,
+    Bath,
+    Play
+}
+
+public enum DogAnimation
+{
+    None = 0,
+    졸려,
+    배고파,
+    오줌마려,
+    씻고싶어,
+    놀고싶어,
+    자기,
+    밥먹기,
+    오줌누기,
+    씻기,
+    놀기
+}
