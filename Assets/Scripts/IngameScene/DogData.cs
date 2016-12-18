eb5fe7302d12b9cfb120dc6303a8934b3d890740
jjr2930 <theoowwp@naver.com>
2016-12-18 21:21:54 +0900

    Create DogData.cs Create DogDesireController.cs

---
+++ Assets/Scripts/IngameScene/DogData.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+[Serializable]
+public class DogData
+{
+    public int 배고픔
+    {
+        get
+        {
+            return _배고픔;
+        }
+        set
+        {
+            _배고픔 = value;
+        }
+    }
+
+    public int 심심함
+    {
+        get
+        {
+            return _심심함;
+        }
+        set
+        {
+            _심심함 = value;
+        }
+    }
+
+    public int 더러움
+    {
+        get
+        {
+            return _더러움;
+        }
+        set
+        {
+            _더러움 = value;
+        }
+    }
+    public int 졸림
+    {
+        get
+        {
+            return _졸림;
+        }
+        set
+        {
+            _졸림 = value;
+        }
+    }
+
+    public int 똥마려움
+    {
+        get
+        {
+            return _똥마려움;
+        }
+        set
+        {
+            _똥마려움 = value;
+        }
+    }
+    public int 멋짐
+    {
+        get
+        {
+            return _멋짐;
+        }
+        set
+        {
+            _멋짐 = value;
+        }
+    }
+
+    [SerializeField]
+    int _배고픔;
+
+    [SerializeField]
+    int _심심함;
+
+    [SerializeField]
+    int _더러움;
+
+    [SerializeField]
+    int _졸림;
+
+    [SerializeField]
+    int _똥마려움;
+
+    [SerializeField]
+    int _멋짐;
+}
+
---
+++ Assets/Scripts/IngameScene/DogDesireController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DesireData
+{
+    public DogDesire desire;
+    public AnimationCurve curve;
+}
+public class DogDesireController : MonoBehaviour
+{
+    [SerializeField]
+    List<DesireData> desireCurve = new List<DesireData>();
+
+    /// <summary>
+    /// 행동량을 이용하여 시간의 변화를 본다.
+    /// </summary>
+    /// <param name="param"></param>
+    public void ListenChangeActPointForHour(object param)
+    {
+        UserDataChangeParameter p = param as UserDataChangeParameter;
+        int hour = p.currentValue % 24;        
+    }
+
+
+    public void LoadCurveDataFromTable()
+    {
+
+    }
+}
