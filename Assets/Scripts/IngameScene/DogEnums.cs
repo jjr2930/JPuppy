f74cb3b89d2d0ddd821497469fe06e98e3f7d977
Joo Jung Yeol <theoowwp@naver.com>
2016-12-21 22:53:46 +0900

    DogEnum 통합

--- Assets/Scripts/IngameScene/DogEnums.cs
+++ Assets/Scripts/IngameScene/DogEnums.cs
@@ -3,6 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+public enum UserDataChange
+{
+    None,
+    ActPoint,
+    MaxActPoint,    
+}
+
+public enum DoAction
+{
+    None,
+    밥먹이기,
+    재우기,
+    씻기기,
+    놀아주기,
+    배변치우기
+}
+
+public enum UIID
+{
+    None,
+    밥먹이기버튼,
+    재우기버튼,
+    씻기기버튼,
+    놀아주기버튼,
+    배변치우기버튼,
+    옷입히기버튼
+}
+
+    
+
 public enum SmartObjectType
 {
     None = 0,
